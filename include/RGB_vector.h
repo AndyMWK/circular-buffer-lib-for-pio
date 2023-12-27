4ecb7784315d2dadb3eaed91bb5e7c5e3094b59d
Andy Kim <111655912+AndyMWK@users.noreply.github.com>
2023-12-26 19:00:27 -0800

    Create RGB_vector.h

---
+++ include/RGB_vector.h
@@ -0,0 +1,9 @@
+namespace RGB_vector {
+
+    float calculate_vector_dist(float x1, float y1, float z1, float x0, float y0, float z0) {
+        float dist = pow(x1-x0, 2) + pow(y1-y0, 2) + pow(z1-z0, 2);
+
+        return sqrt(dist);
+    }
+
+}
